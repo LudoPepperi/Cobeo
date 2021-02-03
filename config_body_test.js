c5be1240693f5d6bbbac5c0943f335f5ac41efc1
LudoPepperi <77826528+LudoPepperi@users.noreply.github.com>
2021-02-03 09:25:55 +0100

    Update config_body_test.js

--- config_body_test.js
+++ config_body_test.js
@@ -47,7 +47,9 @@ var Promotions = [
         buttonText: "Cliquez ici",
         link: 'Transactions/scope_items/{{UUID}}?SearchString=&CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%225332e389-e1f9-421e-bc33-689bdfbca16c%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22DynamicFilter%5C%5C%5C%22:%5C%5C%5C%22Item.MainCategory%5C%5C%5C%22,%5C%5C%5C%22Value%5C%5C%5C%22:%5C%5C%5C%22MOANA%20-%20FLEUR%20DE%20TIARE%5C%5C%5C%22%7D%5C%22%7D%22&TopPadding=0&SearchAll=false',
         image: 'https://ludopepperi.github.io/test/MOANA.jpg'
-    },
+    }
+    ]
+var Promotions2 = [
     {
         title: "DELIRIUM FLORAL",
         buttonText: "Cliquez ici",
@@ -55,7 +57,7 @@ var Promotions = [
         image: 'https://ludopepperi.github.io/test/delirium floral home.png'
     }
     ]
-var Promotions2 = [    
+var Promotions3 = [    
     {
         title: "EAUX DE PARFUM",
         buttonText: "Cliquez ici",
@@ -63,7 +65,7 @@ var Promotions2 = [
         image: 'https://ludopepperi.github.io/test/Présentoir eau parfums.png'
     }
     ]
-var Promotions3 = [
+var Promotions4 = [
     {
         title: "CR\u00c9MES MAINS",
         buttonText: "Cliquez ici",
