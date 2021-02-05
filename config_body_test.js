776f9b95f7ca1d429cb9117b93c7e76bcd191eb1
LudoPepperi <77826528+LudoPepperi@users.noreply.github.com>
2021-02-05 09:54:43 +0100

    Update config_body_test.js

--- config_body_test.js
+++ config_body_test.js
@@ -2,7 +2,7 @@
 var Catalog = 'B2B'
 var blocks_config = {
     'free_shipping': {
-        text: "Frais de port offert \u00e0 partir de 200\u20ac d'achat",
+        text: "Frais de port offert \u00e0 partir de 200\u20ac HT d'achat",
         field: "",
         svg: "https://storage.pepperi.com/General/Icons/truck.svg"
     },
