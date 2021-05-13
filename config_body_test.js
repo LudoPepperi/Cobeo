ccd1660a87fbc2adaf5280f2576ebeaf1a3ff373
LudoPepperi <77826528+LudoPepperi@users.noreply.github.com>
2021-05-13 11:16:43 +0200

    Update config_body_test.js

--- config_body_test.js
+++ config_body_test.js
@@ -9,7 +9,7 @@ var blocks_config = {
     'account_balance': {
         text: "Choix Texte",
         field: "",
-        measure_unit: "unité",
+        measure_unit: "",
         svg: "https://storage.pepperi.com/General/Icons/balance.svg"
     },
     'active-order':
