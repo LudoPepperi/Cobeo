508201cec1e774a835a4b15c850cac3a7dfe2159
LudoPepperi <77826528+LudoPepperi@users.noreply.github.com>
2021-05-15 18:26:45 +0200

    Update config_body_test.js

--- config_body_test.js
+++ config_body_test.js
@@ -8,7 +8,7 @@ var blocks_config = {
     },
     'account_balance': {
         text: "Frais de port offert à partir de",
-        field: "TSAFRANCO",
+        field: calc("TSAFRANCO"/1000000),
         measure_unit: "\u20ac",
         svg: "https://storage.pepperi.com/General/Icons/truck.svg"
     },
