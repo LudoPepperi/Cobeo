7d8b828ad6fc447d49f072063d4b1432250cee4a
LudoPepperi <77826528+LudoPepperi@users.noreply.github.com>
2021-05-18 11:31:44 +0200

    Update config_body_test.js

--- config_body_test.js
+++ config_body_test.js
@@ -195,7 +195,7 @@ var Promotions = [
         title: "Titre 1",
         buttonText: "Cliquez ici",
         link: 'Transactions/scope_items/{{UUID}}?SearchString=&CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%225332e389-e1f9-421e-bc33-689bdfbca16c%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%2299207e51-47b5-4f30-8271-c364a6882050%5C%5C%5C%22%7D%5C%22%7D%22&TopPadding=0&SearchAll=false',
-        image: 'https://ludopepperi.github.io/Corbeo'
+        image: 'https://ludopepperi.github.io/Corbeo/Carrousel - 1 Galan.jpg'
     }
     ]
 var Promotions2 = [
@@ -203,7 +203,7 @@ var Promotions2 = [
         title: "Titre 2",
         buttonText: "Cliquez ici",
         link: 'Transactions/scope_items/{{UUID}}?SearchString=&CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%225332e389-e1f9-421e-bc33-689bdfbca16c%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%221fed1213-9371-4267-8e07-c83c56b057e1%5C%5C%5C%22%7D%5C%22%7D%22&TopPadding=0&SearchAll=false',
-        image: 'https://ludopepperi.github.io/Corbeo'
+        image: 'https://ludopepperi.github.io/Corbeo/Carrousel - 2b.png'
     }
     ]
 var Promotions3 = [    
@@ -211,7 +211,7 @@ var Promotions3 = [
         title: "Titre 3",
         buttonText: "Cliquez ici",
         link: 'Transactions/scope_items/{{UUID}}?SearchString=&CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%225332e389-e1f9-421e-bc33-689bdfbca16c%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%2209fd1d0b-e9f8-414f-9c10-e3e4b72049ee%5C%5C%5C%22%7D%5C%22%7D%22&TopPadding=0&SearchAll=false',
-        image: 'https://ludopepperi.github.io/Corbeo'
+        image: 'https://ludopepperi.github.io/Corbeo/Carrousel - 3.jpg'
     }
     ]
 var Promotions4 = [
@@ -219,7 +219,7 @@ var Promotions4 = [
         title: "Titre 4",
         buttonText: "Cliquez ici",
         link: 'Transactions/scope_items/{{UUID}}?SearchString=&CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%2202837f2a-b9c3-4bbd-a455-5c090d211c89%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%2299207e51-47b5-4f30-8271-c364a6882050%5C%5C%5C%22%7D%5C%22%7D%22&TopPadding=0&SearchAll=false',
-        image: 'https://ludopepperi.github.io/Corbeo'
+        image: 'https://ludopepperi.github.io/Corbeo/Carrousel - 4.jpg'
     }
 ]
 
@@ -227,7 +227,7 @@ var CaruselData = [
     {
 
         title: '',
-        imageURL: 'https://ludopepperi.github.io/Corbeo',
+        imageURL: 'https://ludopepperi.github.io/Corbeo/Title (1).png',
         description: '',
         buttonText: '',
         time: 5000,
@@ -236,7 +236,7 @@ var CaruselData = [
     {
 
         title: '',
-        imageURL: 'https://ludopepperi.github.io/Corbeo ',
+        imageURL: 'https://ludopepperi.github.io/Corbeo/Title (2).png ',
         description: '',
         buttonText: '',
         time: 5000,
@@ -245,7 +245,7 @@ var CaruselData = [
     {
 
         title: '',
-        imageURL: 'https://ludopepperi.github.io/Corbeo',
+        imageURL: 'https://ludopepperi.github.io/Corbeo/Title (3).png',
         description: '',
         buttonText: '',
         time: 5000,
@@ -254,7 +254,7 @@ var CaruselData = [
     {
 
         title: '',
-        imageURL: 'https://ludopepperi.github.io/Corbeo',
+        imageURL: 'https://ludopepperi.github.io/Corbeo/Title (4).png',
         description: '',
         buttonText: '',
         time: 5000,
