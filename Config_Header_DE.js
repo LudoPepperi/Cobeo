99e6eff2608d7011fd697a1ab2c311e250966c9d
LudoPepperi <77826528+LudoPepperi@users.noreply.github.com>
2021-11-05 16:24:28 +0100

    Update Config_Header_DE.js

--- Config_Header_DE.js
+++ Config_Header_DE.js
@@ -1,5 +1,5 @@
 var Transaction = 'Sales Order DE';
-var Catalog = 'Catalog Buyers';
+var Catalog = 'Catalog B2B';
 const logo = 'https://ludopepperi.github.io/Corbeo/CorbeoLogo.png';
 const LeftMenu = [
 
